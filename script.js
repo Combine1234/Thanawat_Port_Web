2d0d3ecaf597985a6be13b509efd558083e51dcb
Uncle Fronk <Thanawatsukamporn@gmail.com>
2025-08-29 22:21:11 +0700

    main

--- script.js
+++
@@ -1,7 +0,0 @@
-// Highlight active link in nav
-const currentPage = location.pathname.split("/").pop();
-document.querySelectorAll("nav a").forEach(link => {
-    if (link.getAttribute("href") === currentPage) {
-        link.style.color = "#ff9800";
-    }
-});
